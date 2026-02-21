dfaa686abc35fe6636253392daf92cab1df93f00
ajayingle7 <79854509+ajayingle7@users.noreply.github.com>
2026-02-21 09:15:00 +0530

    Add hello world print at end of Django settings

--- onetoone/onetoone/settings.py
+++ onetoone/onetoone/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'onetoone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,11 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ['static']
+STATICFILES_DIRS = [BASE_DIR.parent / 'static']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+print('hello world')
